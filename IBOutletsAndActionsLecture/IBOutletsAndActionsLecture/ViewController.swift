a2d0720a8ad15c493af0bf978b548ec4d3b50cbe
HeatherConnery <heatherconnery@gmail.com>
2015-10-28 11:46:28 -0400

    lecture complete

--- IBOutletsAndActionsLecture/IBOutletsAndActionsLecture/ViewController.swift
+++ IBOutletsAndActionsLecture/IBOutletsAndActionsLecture/ViewController.swift
@@ -8,8 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // data model for list
+    var myTaskList = [String]()
+    
 
+    @IBOutlet weak var myTextField: UITextField!
+    
+    @IBAction func myButton(sender: AnyObject) {
+        self.myTaskList.append(self.myTextField.text!)
+        self.myTableView.reloadData()
+    }
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    // delegate methods for tableView
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.myTaskList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        // in closures we will need to use the self to explicitly reference that it is owned by the view controller, here is is optional:
+        cell.textLabel?.text = self.myTaskList[indexPath.row]
+        return cell
+    }
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
